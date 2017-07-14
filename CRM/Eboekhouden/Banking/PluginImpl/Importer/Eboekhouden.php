e1855ae088b727d857e916b2cce2ca5eec763936
Daniël <daniel@qaleido.nl>
2017-07-14 09:45:58 +0200

    Update Eboekhouden.php

--- CRM/Eboekhouden/Banking/PluginImpl/Importer/Eboekhouden.php
+++ CRM/Eboekhouden/Banking/PluginImpl/Importer/Eboekhouden.php
@@ -106,7 +106,6 @@ class CRM_Eboekhouden_Banking_PluginImpl_Importer_Eboekhouden extends CRM_Bankin
     // begin
     $config = $this->_plugin_config;
     $this->reportProgress(0.0, sprintf("Creating SOAP connection with username '%s'...", $config->username));
-    $this->reportProgress(0.0, sprintf("Config: '%s'", serialize($config)));
     $soapClient = new SoapClient("https://soap.e-boekhouden.nl/soap.asmx?WSDL");
     $line_nr = 1; // we want to skip the header (not yet implemented)
 
@@ -142,7 +141,7 @@ class CRM_Eboekhouden_Banking_PluginImpl_Importer_Eboekhouden extends CRM_Bankin
         $payment_lines = $Mutations->cMutatieList;
       }
     } else {
-      $payment_lines = unserialize(gzuncompress(base64_decode($debug_object)));
+      $payment_lines = unserialize(gzuncompress(base64_decode($config->debug_object)));
     }
     
     $batch = $this->openTransactionBatch();
